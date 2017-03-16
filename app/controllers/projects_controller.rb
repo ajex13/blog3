4482a7c05f0bd9419be100518f31b4086edb76e1
ajex13 <ajex13@gmail.com>
2017-03-17 00:24:32 +0530

    Project part basic styling and functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+before_action :project_find, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all.order("created_at DESC")
+  end
+
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: " New Project saved "
+    else
+      render 'new', notice: "Oh no, Project was not saved!"
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private
+
+def project_params
+  params.require(:project).permit(:title, :description, :link)
+end
+
+def project_find
+  @project = Project.find(params[:id])
+end
+
+end
